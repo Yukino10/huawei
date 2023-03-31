950fd3baa7131168aa6990334a6e16e7884c57e6
zhanglong <9423220+zhanglong2021@user.noreply.gitee.com>
2023-03-31 21:07:31 +0800

    feat: bitmap

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include<bits/stdc++.h>
+#include "unistd.h"
 using namespace std;
 typedef long long ll;
 const int NMAX = 200005;
 const int PMAX = 100;
-class solve {
-public:
-
-};
 int N, M, T, P, D;
 ll Y, DC, EC;
 map<pair<int, int>, int>minDis;
@@ -17,14 +14,42 @@ struct Edge{
     int u, v, d;
 }edge[NMAX];
 vector<int>g[NMAX];
-bitset<NMAX>vis;
+
+struct BitMap{
+    char a[NMAX];
+    bool operator[](int index){
+        return (a[index / 8] >> (index % 8)) & 1;
+    }
+    void set(int index){
+        a[index / 8] |=  1 << (index % 8);
+    }
+    void reset(int index){
+        a[index / 8] &=  ~(1 << (index % 8));
+    }
+    void clear(int tot){
+        memset(a, 0, tot / 8 + 1);
+    }
+    void clear(){
+        memset(a, 0, sizeof(a));
+    }
+};
+BitMap vis;
 int group[NMAX];
+bool edgePMAP[NMAX][PMAX];
+int edgePRE[NMAX];
+struct ANS{
+    int p;
+    vector<int>edge;
+    vector<int>costD;
+}Ans[NMAX];
+vector<int> gShrink[NMAX];
+
 
 int bfsPRE(int s, int t){
-    vis.reset();
+    vis.clear(N);
     queue<int>q;
     q.push(s);
-    vis[s] = true;
+    vis.set(s);
     int dis = 0;
     while(q.size()){
         int tot = q.size();
@@ -34,19 +59,18 @@ int bfsPRE(int s, int t){
             q.pop();
             for(auto i : g[x]){
                 int y = x ^ edge[i].u ^ edge[i].v;
-                if(!vis[y])vis[y] = true, q.push(y);
+                if(!vis[y])vis.set(y), q.push(y);
             }
         }
         dis++;
     }
 }
 
-bool edgePMAP[NMAX][PMAX];
 int bfsSEA(int s, int t, int p){
-    vis.reset();
+    vis.clear(N);
     queue<int>q;
     q.push(s);
-    vis[s] = true;
+    vis.set(s);
     int dis = 0;
     while(q.size()){
         int tot = q.size();
@@ -56,7 +80,7 @@ int bfsSEA(int s, int t, int p){
             q.pop();
             for(auto i : g[x]){
                 int y = x ^ edge[i].u ^ edge[i].v;
-                if(!vis[y] && !edgePMAP[i][p])vis[y] = true, q.push(y);
+                if(!vis[y] && !edgePMAP[i][p])vis.set(y), q.push(y);
             }
         }
         dis++;
@@ -64,18 +88,11 @@ int bfsSEA(int s, int t, int p){
     return 0;
 }
 
-int edgePRE[NMAX];
-struct ANS{
-    int p;
-    vector<int>edge;
-    vector<int>costD;
-}Ans[NMAX];
-
 void bfsSEL(int s, int t, int p){
-    vis.reset();
+    vis.clear(N);
     queue<int>q;
     q.push(s);
-    vis[s] = true;
+    vis.set(s);
     while(q.size()){
         int tot = q.size();
         while(tot--){
@@ -85,7 +102,7 @@ void bfsSEL(int s, int t, int p){
             for(auto i : g[x]){
                 int y = x ^ edge[i].u ^ edge[i].v;
                 if(!vis[y] && !edgePMAP[i][p]){
-                    vis[y] = true, q.push(y);
+                    vis.set(y), q.push(y);
                     edgePRE[y] = i;
                 }
             }
@@ -111,25 +128,24 @@ void SelectEdge(int s, int t, int p, int stId){
 }
 
 void dfsShrinkPoint(int v, int p, int count){
-    vis[v] = true;
+    vis.set(v);
     group[v] = count;
     for(auto i : g[v]){
         int u = v ^ edge[i].u ^ edge[i].v;
         if(!vis[u] && !edgePMAP[i][p]){
-            vis[u] = true;
+            vis.set(u);
             group[u] = count;
             dfsShrinkPoint(u, p, count);
         }
     }
 }
 
-vector<int> gShrink[NMAX];
 int bfsSHR(int s, int t){
-    vis.reset();
+    vis.clear(N);
     queue<int>q;
     q.push(s);
     int dis = 0;
-    vis[s] = true;
+    vis.set(s);
     while(q.size()){
         int tot = q.size();
         while(tot--){
@@ -139,7 +155,7 @@ int bfsSHR(int s, int t){
             for(auto i : gShrink[x]){
                 int y = x ^ group[edge[i].v] ^ group[edge[i].u];
                 if(!vis[y]){
-                    vis[y] = true;
+                    vis.set(y);
                     q.push(y);
                     edgePRE[y] = i;
                 }
@@ -151,7 +167,7 @@ int bfsSHR(int s, int t){
 
 
 vector<int> GetAddEdgeP(int s, int t, int p){
-    vis.reset();
+    vis.clear(N);
     int count = 0;
     for(int i = 0; i < N; i++){
         if(!vis[i]){
@@ -241,16 +257,19 @@ void GetCost(){
     }
 }
 
-void Test(){
-
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
+void inData(){
     cin >> N >> M >> T >> P >> D;
     for(int i = 0; i < M; i++){
         cin >> edge[i].u >> edge[i].v >> edge[i].d;
+    }
+    for(int i = 0; i < T; i++){
+        cin >> st[i].s >> st[i].t;
+    }
+}
+
+void Run(){
+
+    for(int i = 0; i < M; i++){
         if(!minDis[{edge[i].u, edge[i].v}])minDis[{edge[i].u, edge[i].v}] = edge[i].d;
         if(!minDis[{edge[i].v, edge[i].u}])minDis[{edge[i].v, edge[i].u}] = edge[i].d;
         minDis[{edge[i].v, edge[i].u}] = min({minDis[{edge[i].v, edge[i].u}], minDis[{edge[i].u, edge[i].v}], edge[i].d});
@@ -259,21 +278,21 @@ int main(){
         g[edge[i].v].push_back(i);
     }
     for(int i = 0; i < T; i++){
-        cin >> st[i].s >> st[i].t;
         st[i].id = i;
         st[i].minDis = bfsPRE(st[i].s, st[i].t);
     }
-
-
     sort(st, st + T, [](ST x, ST y){
         return x.minDis > y.minDis;
     });
     random_shuffle(st, st + T);
     for(int i = 0; i < T; i++){
         Search(st[i].s, st[i].t, st[i].id);
+        //cout << i << endl;
     }
     GetCost();
+}
 
+void outAns(){
     cout << Y << endl;
     for(int i = 0; i < Y; i++){
         cout << edge[M - Y + i].v << ' ' << edge[M - Y + i].u << endl;
@@ -284,10 +303,28 @@ int main(){
         for(auto j : Ans[i].costD)cout << j << ' ';
         cout << endl;
     }
-//    cout << "AC : " << ' ' << Y << ' ' << Y * 1000000 << endl;
-//    cout << "DC : " << ' ' << DC << ' ' << DC * 100 << endl;
-//    cout << "EC : " << ' ' << EC << ' ' << EC << endl;
-//    cout << "ALL COST : " << Y * 1000000 + DC * 100 + EC << endl;
+}
+
+void outCost(){
+    cout << "AC : " << ' ' << Y << ' ' << Y * 1000000 << endl;
+    cout << "DC : " << ' ' << DC << ' ' << DC * 100 << endl;
+    cout << "EC : " << ' ' << EC << ' ' << EC << endl;
+    cout << "ALL COST : " << Y * 1000000 + DC * 100 + EC << endl;
+}
+
+void Work(){
+    inData();
+    Run();
+    outAns();
+}
+
+void Test();
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //Work();
+    Test();
 }
 /*
 7 10 6 4 6
@@ -310,3 +347,39 @@ int main(){
 
 
  */
+
+
+void Test(){
+    N = 500, M = 1000, T = 10000, P = 80, D = 100;
+    set<pair<int, int>>se;
+    for(int i = 1; i < N; i++){
+        se.insert({rand(), i});
+    }
+    int pre = 0;
+    int count = 0;
+    while(!se.empty()){
+        int v = se.begin()->second;
+        se.erase(se.begin());
+        edge[count].u = pre, edge[count].v = v;
+        edge[count].d = rand() % D;
+        count++;
+        pre = v;
+    }
+    while(count < M){
+        int x = rand() % N, y = rand() % N;
+        while(x == y)y = rand() % N;
+        edge[count].u = x, edge[count].v = y;
+        edge[count].d = rand() % D;
+        count++;
+    }
+    for(int i = 0; i < T; i++){
+        int x = rand() % N, y = rand() % N;
+        while(x == y)y = rand() % N;
+        st[i].s = x, st[i].t = y;
+    }
+    double start = clock();
+    cout << getpid()  << endl;
+    Run();
+    outCost();
+    cout << (int)(clock() - start) / 1000 << endl;
+}
